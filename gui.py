4ca385d4b80e8d357737f18779a96ac8c1eded8d
santoiio <santoyo96@live.com>
2025-01-03 02:09:43 -0600

    Create a static web interface. Create a desktop executable.

--- gui.py
+++ gui.py
@@ -3,17 +3,30 @@ from time import strftime
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
 
-sg.theme("Black")
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
+
+sg.theme("DarkBlue6")
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter to-do", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=2,
+                       image_source=r"C:\Users\santo\OneDrive\Python"
+                                    r"\todo_app\add.png",
+                       mouseover_colors="LightBlue2",
+                       tooltip="Add todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(size=10,
+                            image_source=r"C:\Users\santo\OneDrive\Python"
+                                         r"\todo_app\complete.png",
+                            mouseover_colors="LightBlue2",
+                            key= "Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
@@ -24,6 +37,10 @@ window = sg.Window('My To-Do App',
                    font=('Helvetica',20))
 while True:
     event, values = window.read(timeout=200)
+
+    if event == sg.WIN_CLOSED:
+        break
+
     window["clock"].update(value=strftime("%b %d, %Y %H:%M:%S"))
 
     match event:
@@ -62,8 +79,6 @@ while True:
             break
         case "todos":
             window['todo'].update(value=values['todos'][0])
-        case sg.WIN_CLOSED:
-            break
 
 print("Bye")
-window.close
+window.close()
